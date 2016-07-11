8e5d3e7d784177ea95cb9969364b2825f596888c
Sreekesh D <sreekesh.d@gmail.com>
2016-07-11 14:53:53 +0530

    Minor

--- src/main/java/com/belhopat/backoffice/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/belhopat/backoffice/service/impl/EmployeeServiceImpl.java
@@ -106,7 +106,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 			if (candidate != null) {
 				String employeeName = employee.getEmployeeMaster().getFirstName() + " "
 						+ employee.getEmployeeMaster().getLastName();
-				mailService.sendCandidateRegMail( candidate, true );
+				mailService.sendCandidateRegMail( candidate, true, "" );
 				return new ResponseEntity<String>(employeeName, HttpStatus.OK);
 			}
 		}
