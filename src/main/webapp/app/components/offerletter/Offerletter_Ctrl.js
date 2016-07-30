2790a4b5bcdcd5be5e467262b441c7a57a7c927e
Akhil <Akhil>
2016-07-30 17:49:48 +0530

    commented upload for now

--- src/main/webapp/app/components/offerletter/Offerletter_Ctrl.js
+++ src/main/webapp/app/components/offerletter/Offerletter_Ctrl.js
@@ -255,7 +255,7 @@
         }
         
         vm.downloadOfferLetter = function(){
-            vm.downloadOfferLetterUrl = "api/downloadDocument?empSalId="+vm.offerletter.id;
+            vm.downloadOfferLetterUrl = "api/previewOfferLetter?empSalId="+vm.offerletter.id;
             vm.downloadOfferLetterUrl = Core_HttpRequest.getUrl(url);
             Core_Service.downloadOfferLetter(vm.downloadOfferLetterUrl,"offer-letter"+vm.offerletter.id);                    
         }
