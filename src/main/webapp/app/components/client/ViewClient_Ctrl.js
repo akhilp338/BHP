c707d27692da478499783279f78e778824bea446
Prince <princeg2qburst.com>
2016-08-01 14:24:48 -0700

    commit for employee view

--- src/main/webapp/app/components/client/ViewClient_Ctrl.js
+++ src/main/webapp/app/components/client/ViewClient_Ctrl.js
@@ -1,50 +1,8 @@
 (function () {
     var ViewClient_Ctrl = function ($scope, $uibModalInstance, CLIENT, Core_Service, clientDetails) {
         var vm = this, fields = CLIENT.fieldMapping;
-        vm.template = "<div class = 'candidate-details-wrapper'>"
-        var array = [];
+        vm.template = "<div class = 'candidate-details-wrapper'>";
         vm.viewClient = {};
-
-//		for ( var key in clientDetails) {
-//			if (key == 'id') {
-//				continue;
-//			} else if (clientDetails[key] == undefined
-//					&& clientDetails[key] == null) {
-//				vm.template += "<div class='cat-row'><span class = 'catagory'>"
-//						+ fields[key] + " </span><span class='cat-value'>"
-//						+ '-' + "</span></div>";
-//			} else if (key == 'dob' || key == 'doj' || key == 'createdDate'
-//					|| key == 'updatedDate') {
-//				vm.template += "<div class='cat-row'><span class = 'catagory'>"
-//						+ fields[key] + " </span><span class='cat-value'>"
-//						+ Core_Service.getFormattedDate(clientDetails[key])
-//						+ "</span></div>";
-//			} else if (typeof (clientDetails[key]) == 'object') {
-//				if (key == 'createdBy' || key == 'updatedBy'
-//						|| key == 'deletedBy') {
-//					continue;
-//				} else {
-//					var thisobject = clientDetails[key];
-//					for ( var newKey in thisobject) {
-//						if (newKey == 'id') {
-//							continue;
-//						} else if (fields[newKey] == undefined) {
-//							continue;
-//						}
-//						vm.template += "<div class='cat-row'><span class = 'catagory'>"
-//								+ fields[newKey]
-//								+ " </span><span class='cat-value'>"
-//								+ thisobject[newKey] + "</span></div>";
-//					}
-//				}
-//			} else if (fields[key] == undefined) {
-//				console.log(key);
-//			} else {
-//				vm.template += "<div class='cat-row'><span class = 'catagory'>"
-//						+ fields[key] + " </span><span class='cat-value'>"
-//						+ clientDetails[key] + "</span></div>";
-//			}
-//		}
         var data = clientDetails;
         vm.viewClient["Client Name"] = data.clientName ? data.clientName : "-";
         vm.viewClient["Client Id"] = data.clientId ? data.clientId : "-";
