22ef166e52f6c9408d6d37077f134acb5e1bdf4b
Sujith <sujithkvclt@gmail.com>
2016-09-03 13:59:41 +0530

    added 2nd iteration controllers

---
+++ src/main/java/com/belhopat/backoffice/controller/ReimburseController.java
@@ -0,0 +1,37 @@
+package com.belhopat.backoffice.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.belhopat.backoffice.dto.RequestObject;
+import com.belhopat.backoffice.model.State;
+import com.belhopat.backoffice.service.BaseService;
+
+/**
+ * @author Belhopat dev team Serves as a webservice handler class
+ *
+ */
+@Controller
+@RequestMapping("/api/reimburse")
+public class ReimburseController {
+
+	@Autowired
+	BaseService baseService;
+
+	/**
+	 * @param requestObject
+	 * @return response entity Lookup method fetches states for a country
+	 */
+	@ResponseBody
+	@RequestMapping(value = "/getStatesByCountry", method = RequestMethod.POST)
+	public ResponseEntity<List<State>> getStatesByCountry(@RequestBody RequestObject requestObject) {
+		return baseService.getStatesByCountry(requestObject.getId());
+	}
+}
