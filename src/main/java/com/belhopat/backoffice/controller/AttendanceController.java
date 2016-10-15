cba0deba75ea3dff8dd57bb54fe730bb2b07737d
Sujith <sujithkvclt@gmail.com>
2016-10-15 12:17:53 +0530

    attendence upload

--- src/main/java/com/belhopat/backoffice/controller/AttendanceController.java
+++ src/main/java/com/belhopat/backoffice/controller/AttendanceController.java
@@ -1,5 +1,7 @@
 package com.belhopat.backoffice.controller;
 
+import java.io.IOException;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,11 +10,15 @@ import org.springframework.data.jpa.datatables.mapping.DataTablesOutput;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.belhopat.backoffice.dto.UploadResponse;
 import com.belhopat.backoffice.model.Attendance;
 import com.belhopat.backoffice.service.AttendanceService;
 import com.belhopat.backoffice.service.BaseService;
+import com.belhopat.backoffice.service.ExcelService;
 
 /**
  * @author Belhopat dev team Handler for all client related service calls
@@ -24,6 +30,9 @@ public class AttendanceController {
 
 	@Autowired
 	BaseService baseService;
+
+	@Autowired
+	ExcelService excelService;
 
 	@Autowired
 	AttendanceService attendanceService;
@@ -38,6 +47,13 @@ public class AttendanceController {
 	@RequestMapping(value = "/getAttendances", method = RequestMethod.GET)
 	public DataTablesOutput<Attendance> getAttendances(@Valid DataTablesInput input) {
 		return attendanceService.getAttendances(input);
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "/uploadAttendanceExcel", method = RequestMethod.POST)
+	public UploadResponse uploadAttendanceExcel(@RequestParam("file") MultipartFile file) throws IOException {
+		UploadResponse response = excelService.uploadExcel("ATNDNCE", file);
+		return response;
 	}
 
 }
