d4e3057e8560c32f66755e1afceeca3a82c9a364
Prince <princeg2qburst.com>
2017-04-26 11:09:30 -0700

    commit for settings

---
+++ src/main/webapp/app/components/General/SettingsCtrl.js
@@ -0,0 +1,24 @@
+(function () {
+    var Settings_Ctrl = function ($scope, $rootScope, $state, Core_Service, Core_ModalService, urlConfig) {
+        var vm = this, attendanceTable;
+            $rootScope.isSettings = true;
+            Core_Service.getTabsForSettings().then(function(res){
+                vm.settingsData = res;
+            },function(err){
+                console,log(err)
+            });
+
+            vm.apply = function(){
+                console.log(vm.settingsData);
+            };
+            vm.back = function(){
+                $rootScope.isSettings = false;
+                $state.go('coreuser.dashboard');
+            };
+            vm.processSelection = function(tabs,selectedTabs){
+
+            };
+}
+    Settings_Ctrl.$inject = ["$scope", '$rootScope', '$state', 'Core_Service', 'Core_ModalService', 'urlConfig'];
+    angular.module('coreModule').controller('Settings_Ctrl', Settings_Ctrl);
+})();
