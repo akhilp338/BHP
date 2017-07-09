48d7f064fc0603a7466b4d2a9e1dc3a2078a7313
Prince <princeg2qburst.com>
2017-07-09 05:53:02 -0700

    commit for offer approval page

--- src/main/webapp/app/components/offerletter/Offer_Review_Ctrl.js
+++ src/main/webapp/app/components/offerletter/Offer_Review_Ctrl.js
@@ -19,6 +19,20 @@
         //Core_Service.getOfferReviewDetails($stateParams.taskId).then(function(res){
            // console.log(res);
         //});
+        
+        vm.getDisplayTemplate = function(data, keys){
+            var template = "<div class='reim-summary-container col-md-10 col-lg-10 col-sm-10 col-xs-12'>";
+            for(var i=0; i<keys.length; i++){                
+                if(data[keys[i]]){
+                    template += '<div class="item col-md-6 col-lg-6 col-sm-6 col-xs-12">'+  
+                                '<label class="col-md-6 col-lg-6 col-sm-6 col-xs-12 item-label">'+ keys[i] +
+                                '</label><p class="col-md-6 col-lg-6 col-sm-6 col-xs-12 item-label-desc"> :   '+ data[keys[i]] +
+                                '</p></div>';
+                }
+            }
+            template += '</div>';
+            return template;
+        };
     }
 };
     Offer_Review_Ctrl.$inject = ["$scope", '$state', '$rootScope', 'Core_Service', 'urlConfig', '$stateParams', '$window', 'validationService', 'Core_HttpRequest'];
